ab14376a92f45bb14902c7308c2db90cac48f45c
AssoDePicche <samuelprado730@gmail.com>
2023-05-27 22:26:06 -0300

    feat: implements stringable interface

--- src/StringBuilder/StringBuilder.php
+++ src/StringBuilder/StringBuilder.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace StringBuilder;
 
-final class StringBuilder
+final class StringBuilder implements \Stringable
 {
     public function __construct(private string $content = '')
     {
+    }
+
+    public function __toString(): string
+    {
+        return $this->content;
     }
 }
